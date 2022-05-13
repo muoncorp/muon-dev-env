f4115283ad3e6dac35b7f91f41557521d8a48583
Mewlips <mewlips@muon.co>
2022-05-13 20:15:42 +0900

    step0 for egui

--- step0_build_env.sh
+++ step0_build_env.sh
@@ -3,4 +3,6 @@
 if ! cc --help >/dev/null; then
 	echo 'Install build essentials.'
 	sudo apt install -y build-essential libssl-dev pkg-config
+	echo 'Install packages for egui'
+	sudo apt install libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libspeechd-dev libxkbcommon-dev binaryen
 fi
